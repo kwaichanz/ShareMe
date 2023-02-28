d056c4d6bee0a35b10d8dcdf0015a3367dc95c1e
kwaichanz <alphezelphaz@gmail.com>
2023-03-01 03:19:20 +0700

    second commit

--- shareme_frontend/src/index.jsx
+++ shareme_frontend/src/index.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-import { ReactDOM } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import App from './App';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+    <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </GoogleOAuthProvider>,
+)
