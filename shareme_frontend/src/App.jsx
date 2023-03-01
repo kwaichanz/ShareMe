0b16bac15ce405829c045bed0278d2b5d3e86ede
kwaichanz <alphezelphaz@gmail.com>
2023-03-01 23:36:37 +0700

    adding Google OAuth2

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './container/Home';
 
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello World
-    </h1>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='/*' element={<Home />} />
+      </Routes>
   )
 }
 
