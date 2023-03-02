cbcc1b8995684477b58b1a24444078741e447ecb
kwaichanz <alphezelphaz@gmail.com>
2023-03-02 21:51:55 +0700

    google login implemented

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -5,17 +5,16 @@ import logo from '../assets/logowhite.png'
 import { GoogleLogin, googleLogout, GoogleOAuthProvider } from '@react-oauth/google';
 import jwt_decode from 'jwt-decode'
 
+import { client } from '../client'
+
 const Login = () => {
     const user = false;
-
-    const responseGoogle = (response) => {
-        console.log(response)
-    }
+    const navigate = useNavigate();
 
     const createOrGetuser = async (response) => {
         const decoded = jwt_decode(response.credential)
         localStorage.setItem('user', JSON.stringify(decoded))
-        const {name, email, picture, sub} = decoded;
+        const { name, email, picture, sub } = decoded;
         const doc = {
             _id: sub,
             _type: 'user',
@@ -25,38 +24,37 @@ const Login = () => {
 
         client.createIfNotExists(doc)
             .then(() => {
-                Navigate('/',{replace: true})
+                navigate('/', { replace: true })
             })
     }
 
     return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_TOKEN}>
-        <div className='flex justify-start items-center flex-col h-screen'>
-            <div className='relative w-full h-full'>
-                <video
-                    src={shareVideo}
-                    type='video/mp4'
-                    loop
-                    controls={false}
-                    muted
-                    autoPlay
-                    className='w-full h-full object-cover' />
-                <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-                    <div className='p-5'>
-                        <img src={logo} width="130px" alt="ShareMe" />
-                    </div>
-                    <div className='shadow-2xl'>
-                        <GoogleLogin
-                            onSuccess={responseGoogle}
-                            onError={responseGoogle}
-                            useOneTap
-                            state_cookie_domain='single_host_origin'
-                        />
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_TOKEN}>
+            <div className='flex justify-start items-center flex-col h-screen'>
+                <div className='relative w-full h-full'>
+                    <video
+                        src={shareVideo}
+                        type='video/mp4'
+                        loop
+                        controls={false}
+                        muted
+                        autoPlay
+                        className='w-full h-full object-cover' />
+                    <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+                        <div className='p-5'>
+                            <img src={logo} width="130px" alt="ShareMe" />
+                        </div>
+                        <div className='shadow-2xl'>
+                            <GoogleLogin
+                                onSuccess={createOrGetuser}
+                                onError={(response) => console.log(response)}
+                                state_cookie_domain='single_host_origin'
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </GoogleOAuthProvider>
+        </GoogleOAuthProvider>
     )
 }
 
