b95f6fae55a4c3084f3f7f48f7b74c0bad523f48
kwaichanz <alphezelphaz@gmail.com>
2023-03-06 18:28:01 +0700

    Siebar component finished

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -5,10 +5,19 @@ import { IoIosArrowForward } from 'react-icons/io'
 
 import logo from '../assets/logo.png'
 
-const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transtition-all duration-200 ease-in-out capitalize'
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize'
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize'
+
+const categories = [
+  { name: 'Animals' },
+  { name: 'Wallpapers' },
+  { name: 'Photography' },
+  { name: 'Gaming' },
+  { name: 'Coding' },
+  { name: 'Other' }
+]
 
 const Sidebar = ({ user, closeToggle }) => {
-
   const handleCloseSideBar = () => {
     if (closeToggle) closeToggle(false);
   }
@@ -23,15 +32,39 @@ const Sidebar = ({ user, closeToggle }) => {
         >
           <img src={logo} alt="logo" className='w-full' />
         </Link>
-      </div>
-      <div className='flex flex-col gap-5'>
-        <NavLink
-          to="/"
-          className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
-        >
+        <div className='flex flex-col gap-5'>
+          <NavLink
+            to="/"
+            className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+            onClick={handleCloseSideBar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
 
-        </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover categories</h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+              onClick={handleCloseSideBar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
       </div>
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSideBar}
+        > 
+          <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   )
 }
