b4c586fed3e3a24061f212a647ff8197b792e81d
kwaichanz <alphezelphaz@gmail.com>
2023-05-06 23:48:15 +0700

    adding save functionality

--- shareme_frontend/src/components/MasonryLayout.jsx
+++ shareme_frontend/src/components/MasonryLayout.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Masonry from 'react-masonry-css';
+import React from "react";
+import Masonry from "react-masonry-css";
 
-import Pin from './Pin';
+import Pin from "./Pin";
 
 const breakpointObj = {
   default: 4,
@@ -10,15 +10,16 @@ const breakpointObj = {
   1200: 3,
   1000: 2,
   500: 1,
-}
+};
 
 const MasonryLayout = ({ pins }) => {
   return (
-    <Masonry className='flex animate-slide-fwd' breakpointCols={breakpointObj}>
-      {pins?.map((pin) => <Pin key={pin.id} pin={pin} className="w-max" />)}
-
+    <Masonry className="flex animate-slide-fwd" breakpointCols={breakpointObj}>
+      {pins?.map((pin) => (
+        <Pin key={pin._id} pin={pin} className="w-max" />
+      ))}
     </Masonry>
-  )  
-}
+  );
+};
 
-export default MasonryLayout
+export default MasonryLayout;
--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -14,12 +14,35 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   const navigate = useNavigate();
   const user = fetchUser();
 
-  const alreadySaved = (save?.filter(
-    (item) => item.postedBy._id === user.googleId
-  )).length;
 
-  // 1 [2,3,1] -> [1].length -> 1 -> !1 -> false -> !false -> true
-  // 4 [2,3,1] -> [1].length -> 0 -> !0 -> true -> !true -> false
+  let alreadySaved = !!save?.filter(
+    (item) => item?.postedBy?._id === user?.googleId
+  )?.length;
+
+  const savePin = (id) => {
+    if (!alreadySaved) {
+      setSavingPost(true);
+
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert("after", "save[-1]", [
+          {
+            _key: uuidv4(),
+            userId: user.googleId,
+            postedBy: {
+              _type: "postedBy",
+              _ref: user.googleId,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          window.location.reload();
+          setSavingPost(false);
+        });
+    }
+  };
 
   return (
     <div className="m-2">
@@ -51,10 +74,24 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                   <MdDownloadForOffline />
                 </a>
               </div>
-              {alreadySaved?.length !== 0 ? (
-                <button>Saved</button>
+              {alreadySaved ? (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none"
+                >
+                  {save?.length} Saved
+                </button>
               ) : (
-                <button>Save</button>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    savePin(_id);
+                  }}
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none"
+                >
+                  Save
+                </button>
               )}
             </div>
           </div>
