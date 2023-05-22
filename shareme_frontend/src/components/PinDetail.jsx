ca5b95e5984fbf9218e6cad9e63e6cb7d9e178b9
kwaichanz <alphezelphaz@gmail.com>
2023-05-22 23:56:08 +0700

    reconfigure destination's url for the pins

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -156,7 +156,7 @@ const PinDetail = ({ user }) => {
           </div>
         </div>
       </div>
-      {pins.length > 0 ? (
+      {pins?.length > 0 ? (
         <>
           <h2 className="text-center font-bold text-2xl mt-8 mb-4">
             More like this
