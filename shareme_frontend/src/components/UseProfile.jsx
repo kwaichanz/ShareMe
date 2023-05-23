6931b0afbb442543ceb1534f333dc28348558513
kwaichanz <alphezelphaz@gmail.com>
2023-05-24 00:05:23 +0700

    prep userProfile component

--- shareme_frontend/src/components/UseProfile.jsx
+++ shareme_frontend/src/components/UseProfile.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { AiOutlineLogout } from "react-icons/ai";
+import { useParams, useNavigate } from "react-router-dom";
+import { googleLogout } from "@react-oauth/google";
+
+import {
+  userCreatedPinsQuery,
+  userQuery,
+  userSavedPinsQuery,
+} from "../utils/data";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const UseProfile = () => {
-  return (
-    <div>UseProfile</div>
-  )
-}
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState("Created"); //Created || Saved
+  const [activeBtn, setActiveBtn] = useState("created");
+  const navigate = useNavigate();
+  const { userId } = useParams;
 
-export default UseProfile
+  if (!user) {
+    return <Spinner message={"Loading profile..."} />;
+  }
+
+  return <div>UseProfile</div>;
+};
+
+export default UseProfile;
