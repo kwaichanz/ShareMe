32ecbafe3b2169acab8dcd0a5741bfe4d001ce42
kwaichanz <alphezelphaz@gmail.com>
2023-05-29 01:11:20 +0700

    added redirect to login page at start

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
-import Login from './components/Login';
-import Home from './container/Home';
-
+import Login from "./components/Login";
+import Home from "./container/Home";
+import { fetchUser } from "./utils/fetchUser";
 
 const App = () => {
-  return (
-      <Routes>
-        <Route path='login' element={<Login />} />
-        <Route path='/*' element={<Home />} />
-      </Routes>
-  )
-}
+  const navigate = useNavigate();
 
-export default App
+  useEffect(() => {
+    const user = fetchUser;
+
+    if (!user) navigate("./login");
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
