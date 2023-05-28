0cae0927f8db7df38c0fc8e8787a7a68ab9df58a
kwaichanz <alphezelphaz@gmail.com>
2023-05-28 16:33:33 +0700

    fixed googleOAuth and savedPins display

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -54,7 +54,7 @@ const Home = () => {
             <img src={logo} alt="logo" className="w-28" />
           </Link>
           <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="logo" className="w-28" />
+            <img src={user?.image} alt="logo" className="w-16 rounded-2xl" />
           </Link>
         </div>
         {toggleSideBar && (
